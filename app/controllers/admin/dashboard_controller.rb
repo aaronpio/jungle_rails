d92547df9dba8acbc939bf5dc599c854b4898a55
vagrant <aaronpiolang@gmail.com>
2020-02-06 15:20:08 +0000

    Added database information/counts in Admin Dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
+    @count_of_products = Product.count
+    @count_of_categories = Category.count
   end
 end
