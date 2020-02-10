f9694d2ae41ae2256c829c3710e519e6f61233ee
vagrant <aaronpiolang@gmail.com>
2020-02-10 21:25:10 +0000

    Completed rspec product unit tests for the form validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,20 +3,46 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    @category = Category.new(name: "toys")
-    @product = Product.new(name: "pogostick", price_cents: 1000, quantity: 100, category: @category)
+    mock_category = Category.new(name: "toys")
+    mock_product = Product.new(name: "pogostick", price_cents: 1000, quantity: 100, category: mock_category)
 
-    puts @category.name
-    puts @product.name
+    
+    it 'should be good if we give it what it wants' do
+      expect(mock_product).to be_valid
+      mock_product.save
+      expect(mock_product.errors.full_messages).to eql([])
+    end
+
     #validates :name, presence: true
-    it 'should have a name'
+    it 'should be given a name' do
+    mock_product.name = nil
+    mock_product.save
+    expect(mock_product.errors.full_messages).to eql(["Name can't be blank"])
+    end
+
     #validates :price, presence: true
+    it 'should be given a price' do
+      mock_product.price_cents = nil
+      mock_product.save
+      expect(mock_product.errors.full_messages).to include("Price cents is not a number")
+    end
+
     #validates :quantity, presence: true
+    it 'should be given a quantity' do
+      mock_product.quantity = nil
+      mock_product.save
+      expect(mock_product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
     #validates :category, presence: true
+    it 'should be given a category' do
+      mock_product.category = nil
+      mock_product.save
+      expect(mock_product.errors.full_messages).to include("Category can't be blank")
+    end
 
   end
     
-  # validation tests/examples here
 end
   
 
